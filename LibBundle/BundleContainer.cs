ea8c2c2de85036e5f9f6b10d28b708a7c1f077ac
aianlinb <ianlin0110g@gmail.com>
2020-09-24 18:03:35 +0800

    Add files via upload

---
+++ LibBundle/BundleContainer.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LibBundle
+{
+    public class BundleContainer
+    {
+        [System.Runtime.InteropServices.DllImport("oo2core_8_win64.dll")]
+        public static extern int OodleLZ_Decompress(byte[] buffer, int bufferSize, byte[] result, long outputBufferSize, int a, int b, int c, IntPtr  d, long e, IntPtr f, IntPtr g, IntPtr h, long i, int ThreadModule);
+        [System.Runtime.InteropServices.DllImport("oo2core_8_win64.dll")]
+        public static extern int OodleLZ_Compress(ENCODE_TYPES format, byte[] buffer, long bufferSize, byte[] outputBuffer, COMPRESSTION_LEVEL level, IntPtr opts, long offs, long unused, IntPtr scratch, long scratch_size);
+        public enum ENCODE_TYPES : uint
+        {
+            LZH = 0,
+            LZHLW = 1,
+            LZNIB = 2,
+            NONE = 3,
+            LZB16 = 4,
+            LZBLW = 5,
+            LZA = 6,
+            LZNA = 7,
+            KRAKEN = 8,
+            MERMAID = 9,
+            BITKNIT = 10,
+            SELKIE = 11,
+            HYDRA = 12,
+            LEVIATHAN = 13
+        }
+
+        public enum COMPRESSTION_LEVEL : uint
+        {
+            None,
+            SuperFast,
+            VeryFast,
+            Fast,
+            Normal,
+            Optimal1,
+            Optimal2,
+            Optimal3,
+            Optimal4,
+            Optimal5
+        }
+
+        public string path;
+        public long offset = 0;
+        public int uncompressed_size;
+        public int data_size;
+        public int head_size;
+        public ENCODE_TYPES encoder;
+        public int unknown;
+        public long size_decompressed;
+        public long size_compressed;
+        public int entry_count;
+        public int chunk_size;
+        public int unknown3;
+        public int unknown4;
+        public int unknown5;
+        public int unknown6;
+        internal byte[] dataToSave;
+
+        //For UnPacking
+        public BundleContainer(string path)
+        {
+            this.path = path;
+            var br = new BinaryReader(File.OpenRead(path));
+            Initialize(br);
+            br.Close();
+        }
+
+        //For UnPacking
+        public BundleContainer(BinaryReader br)
+        {
+            Initialize(br);
+        }
+
+        //For Packing
+        public BundleContainer(byte[] data)
+        {
+            offset = 0;
+            size_decompressed = uncompressed_size = data.Length;
+            encoder = ENCODE_TYPES.LEVIATHAN;
+            entry_count = (data.Length % 262144) == 0 ? data.Length / 262144 : (data.Length / 262144) + 1;
+            head_size = entry_count * 4 + 48;
+            chunk_size = 262144;
+            unknown = 1;
+            unknown3 = unknown4 = unknown5 = unknown6 = 0;
+            dataToSave = data;
+        }
+        
+        private void Initialize(BinaryReader br)
+        {
+            offset = br.BaseStream.Position;
+            uncompressed_size = br.ReadInt32();
+            data_size = br.ReadInt32();
+            head_size = br.ReadInt32();
+            encoder = (ENCODE_TYPES)br.ReadInt32();
+            unknown = br.ReadInt32();
+            size_decompressed = (int)br.ReadInt64();
+            size_compressed = br.ReadInt64();
+            entry_count = br.ReadInt32();
+            chunk_size = br.ReadInt32();
+            unknown3 = br.ReadInt32();
+            unknown4 = br.ReadInt32();
+            unknown5 = br.ReadInt32();
+            unknown6 = br.ReadInt32();
+        }
+
+        //UnPacking
+        public MemoryStream Read(BinaryReader br = null)
+        {
+            if (dataToSave != null)
+                throw new NotSupportedException("Save() cannot be called when it's implemented using a constructor with data and path parameters");
+            if (br == null)
+                if (path == null)
+                    throw new ArgumentException("BundleContainer implemented using a constructor with BinaryReader parameters must include the br parameter when calling Read()", "br");
+                else
+                    br = new BinaryReader(File.OpenRead(path));
+            br.BaseStream.Seek(offset + 60, SeekOrigin.Begin);
+
+            var chunks = new int[entry_count];
+            for (int i = 0; i < entry_count; i++)
+            {
+                chunks[i] = br.ReadInt32();
+            }
+            
+            var data = new MemoryStream(uncompressed_size);
+
+            for (int i = 0; i < entry_count; i++)
+            {
+                var b = br.ReadBytes(chunks[i]);
+                int size = (i + 1 == entry_count) ? uncompressed_size - (chunk_size * (entry_count - 1)) : chunk_size;
+                var toSave = new byte[size + 64];
+                OodleLZ_Decompress(b, b.Length, toSave, toSave.Length, 0, 0, 0, IntPtr.Zero, 0, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, 0, 3);
+                data.Write(toSave, 0, size);
+            }
+            br.Close();
+            return data;
+        }
+
+        //Packing
+        public virtual void Save(string path)
+        {
+            if (dataToSave == null)
+                throw new NotSupportedException("Save() only can be called when it's implemented using a constructor with \"byte[] data\" parameters");
+            this.path = path;
+            var bw = new BinaryWriter(File.Create(path));
+            bw.BaseStream.Seek(offset + 60 + (entry_count*4), SeekOrigin.Begin);
+            data_size = 0;
+            var ms = new MemoryStream(dataToSave);
+            var chunks = new int[entry_count];
+            for (int i = 0; i < entry_count - 1; i++)
+            {
+                var b = new byte[chunk_size];
+                ms.Read(b, 0, chunk_size);
+                var by = new byte[b.Length + 548];
+                var l = OodleLZ_Compress(ENCODE_TYPES.LEVIATHAN, b, b.Length, by, COMPRESSTION_LEVEL.Normal, IntPtr.Zero, 0, 0, IntPtr.Zero, 0);
+                data_size += chunks[i] = l;
+                bw.Write(by, 0, l);
+            }
+            var b2 = new byte[dataToSave.Length - (entry_count - 1) * chunk_size];
+            ms.Read(b2, 0, b2.Length);
+            var by2 = new byte[b2.Length + 548];
+            var l2 = OodleLZ_Compress(ENCODE_TYPES.LEVIATHAN, b2, b2.Length, by2, COMPRESSTION_LEVEL.Normal, IntPtr.Zero, 0, 0, IntPtr.Zero, 0);
+            data_size += chunks[entry_count - 1] = l2;
+            bw.Write(by2, 0, l2);
+
+            bw.BaseStream.Seek(offset + 60, SeekOrigin.Begin);
+            for (int i = 0; i < entry_count; i++)
+            {
+                bw.Write(chunks[i]);
+            }
+
+            size_compressed = data_size;
+            bw.BaseStream.Seek(offset, SeekOrigin.Begin);
+            bw.Write(uncompressed_size);
+            bw.Write(data_size);
+            bw.Write(head_size);
+            bw.Write((uint) encoder);
+            bw.Write(unknown);
+            bw.Write(size_decompressed);
+            bw.Write(size_compressed);
+            bw.Write(entry_count);
+            bw.Write(chunk_size);
+            bw.Write(unknown3);
+            bw.Write(unknown4);
+            bw.Write(unknown5);
+            bw.Write(unknown6);
+
+            bw.Flush();
+            bw.Close();
+            ms.Close();
+        }
+    }
+}
---
+++ VisualBundle/MainWindow.xaml.cs
@@ -0,0 +1,414 @@
+﻿using LibBundle;
+using LibBundle.Records;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace VisualBundle
+{
+    public partial class MainWindow : Window
+    {
+        public IndexContainer ic;
+        private FileRecord moveF;
+        private TreeViewItem moveD;
+        private HashSet<BundleRecord> changed = new HashSet<BundleRecord>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("LibBundle.dll"))
+            {
+                MessageBox.Show("File not found: LibBundle.dll", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+                return;
+            }
+            if (!File.Exists("oo2core_8_win64.dll"))
+            {
+                MessageBox.Show("File not found: oo2core_8_win64.dll", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+                return;
+            }
+            var ofd = new OpenFileDialog
+            {
+                DefaultExt = "bin",
+                FileName = "_.index.bin",
+                Filter = "GGG Bundle index|_.index.bin"
+            };
+            S:
+            if (ofd.ShowDialog() == true)
+            {
+                if (ofd.SafeFileName != "_.index.bin")
+                {
+                    MessageBox.Show("You must select _.index.bin!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    goto S;
+                }
+            }
+            else
+            {
+                Close();
+                return;
+            }
+
+            Environment.CurrentDirectory = Path.GetDirectoryName(ofd.FileName);
+            var Tree = new Dictionary<string, TreeViewItem>();
+            ic = new IndexContainer("_.index.bin");
+            foreach (var b in ic.Bundles)
+                if (File.Exists(b.Name))
+                    BuildTree(Tree, b.Name, b);
+            foreach (var tvi in Tree.Values)
+                View1.Items.Add(tvi);
+        }
+
+        private void OnTreeViewItemExpanded(object sender, RoutedEventArgs e)
+        {
+            var tvi = e.OriginalSource as TreeViewItem;
+            if (tvi.Items != null)
+            {
+                tvi.Items.Clear();
+                foreach(var c in ((Dictionary<string, TreeViewItem>)tvi.Tag).Values)
+                    tvi.Items.Add(c);
+            }
+        }
+
+        private void OnTreeView1SelectedChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var tvi = (TreeViewItem)e.NewValue;
+            if (tvi == null) //No Selected
+            {
+                ButtonAdd.IsEnabled = false;
+                return;
+            }
+            var br = tvi.Tag as BundleRecord;
+            if(br == null) //Selected Directory
+                ButtonAdd.IsEnabled = false;
+            else //Selected Bundle File
+            {
+                if (moveD != null)
+                    MoveD(br);
+                if (moveF != null)
+                    MoveF(br);
+                offsetView.Text = br.indexOffset.ToString();
+                sizeView.Text = br.Size.ToString();
+                noView.Text = br.bundleIndex.ToString();
+                var Tree = new Dictionary<string, TreeViewItem>();
+                foreach (var f in br.Files)
+                    BuildTree(Tree, ic.Hashes[f.Hash], f);
+                View2.Items.Clear();
+                foreach (var t in Tree.Values)
+                    View2.Items.Add(t);
+                ButtonAdd.IsEnabled = true;
+            }
+        }
+
+        private void OnTreeView2SelectedChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var tvi = (TreeViewItem)e.NewValue;
+            if (tvi == null) //No Selected
+            {
+                ButtonExport.IsEnabled = false;
+                ButtonReplace.IsEnabled = false;
+                ButtonMove.IsEnabled = false;
+                ButtonOpen.IsEnabled = false;
+                return;
+            }
+            var fr = tvi.Tag as FileRecord;
+            if (fr == null) //Selected Directory
+            {
+                ButtonExport.IsEnabled = true;
+                ButtonReplace.IsEnabled = false;
+                ButtonMove.IsEnabled = true;
+                ButtonOpen.IsEnabled = false;
+            }
+            else //Selected File
+            {
+                BOffsetView.Text = fr.Offset.ToString();
+                IOffsetView.Text = fr.indexOffset.ToString();
+                fSizeView.Text = fr.Size.ToString();
+                ButtonExport.IsEnabled = true;
+                ButtonReplace.IsEnabled = true;
+                ButtonMove.IsEnabled = true;
+                ButtonOpen.IsEnabled = true;
+            }
+        }
+
+        public StackPanel TreeItem(string path, ImageSource icon)
+        {
+            var sp = new StackPanel { Orientation = Orientation.Horizontal };
+            sp.Children.Add(new Image {Source=icon, Width = 20, Height = 20 });
+            sp.Children.Add(new TextBlock {Text=path, FontSize=16});
+            return sp;
+        }
+
+        public void BuildTree(Dictionary<string, TreeViewItem> tree, string path, object file)
+        {
+            var paths = path.Split('/');
+            TreeViewItem parent = null;
+            for (int i = 0; i < paths.Length; i++)
+            {
+                if (parent == null)
+                {
+                    if (tree.ContainsKey(paths[i]))
+                        parent = tree[paths[i]];
+                    else
+                    {
+                        var isFile = i + 1 == paths.Length;
+                        var tvi = new TreeViewItem { Header = TreeItem(paths[i], isFile ? Properties.Resources.file : Properties.Resources.dir) };
+                        if (isFile)
+                            tvi.Tag = file;
+                        else
+                        {
+                            tvi.Tag = new Dictionary<string, TreeViewItem>();
+                            tvi.Items.Add("Loading . . .");
+                        }
+                        parent = tree[paths[i]] = tvi;
+                    }
+                }
+                else
+                {
+                    var pr = (Dictionary<string, TreeViewItem>)parent.Tag;
+                    if (pr.ContainsKey(paths[i]))
+                        parent = pr[paths[i]];
+                    else
+                    {
+                        var isFile = i + 1 == paths.Length;
+                        var tvi = new TreeViewItem { Header = TreeItem(paths[i], isFile ? Properties.Resources.file : Properties.Resources.dir) };
+                        if (isFile)
+                            tvi.Tag = file;
+                        else
+                        {
+                            tvi.Tag = new Dictionary<string, TreeViewItem>();
+                            tvi.Items.Add("Loading . . .");
+                        }
+                        parent = pr[paths[i]] = tvi;
+                    }
+                }
+            }
+        }
+
+        private void OnButtonExportClick(object sender, RoutedEventArgs e)
+        {
+            var tvi = View2.SelectedItem as TreeViewItem;
+            if (tvi == null)
+                return;
+            var f = tvi.Tag as FileRecord;
+            if (f != null) //Selected File
+            {
+                var ofd = new SaveFileDialog
+                {
+                    FileName = Path.GetFileName(ic.Hashes[f.Hash]),
+                    Filter = "All Files|*.*"
+                };
+                if (ofd.ShowDialog() == true)
+                {
+                    File.WriteAllBytes(ofd.FileName, f.Read());
+                    MessageBox.Show("Saved: " + f.Size + " Bytes" + Environment.NewLine + ofd.FileName, "Done");
+                }
+            }
+            else //Selected Directory
+            {
+                var ofd = new SaveFileDialog
+                {
+                    FileName = ((TextBlock)((StackPanel)tvi.Header).Children[1]).Text
+                };
+                if (ofd.ShowDialog() == true)
+                {
+                    var path = Path.GetFileNameWithoutExtension(ofd.FileName);
+                    var fis = tvi.Tag as Dictionary<string, TreeViewItem>;
+                    MessageBox.Show("Exported " + ExportDir(fis, path).ToString() + " Files", "Done");
+                }
+            }
+        }
+
+        private int ExportDir(Dictionary<string, TreeViewItem> fis, string path)
+        {
+            int count = 0;
+            Directory.CreateDirectory(path);
+            foreach (var fi in fis)
+            {
+                var fr = fi.Value.Tag as FileRecord;
+                if (fr == null) // is directory
+                {
+                    Directory.CreateDirectory(path + "\\" + fi.Key);
+                    count += ExportDir((Dictionary<string, TreeViewItem>)fi.Value.Tag, path + "\\" + fi.Key);
+                }
+                else // is file
+                {
+                    File.WriteAllBytes(path + "\\" + fi.Key, fr.Read());
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void OnButtonReplaceClick(object sender, RoutedEventArgs e)
+        {
+            var tvi = View2.SelectedItem as TreeViewItem;
+            if (tvi == null)
+                return;
+            var f = tvi.Tag as FileRecord;
+            if (f != null) //Selected File
+            {
+                var ofd = new OpenFileDialog { FileName = Path.GetFileName(ic.Hashes[f.Hash]) };
+                if (ofd.ShowDialog() == true)
+                {
+                    f.Write(File.ReadAllBytes(ofd.FileName));
+                    changed.Add(f.bundleRecord);
+                    ButtonSave.IsEnabled = true;
+                    MessageBox.Show("Imported " + f.Size.ToString() + " Bytes", "Done");
+                }
+            }
+        }
+
+        private void OnButtonMoveClick(object sender, RoutedEventArgs e)
+        {
+            var tvi = View2.SelectedItem as TreeViewItem;
+            if (tvi == null)
+                return;
+            var f = tvi.Tag as FileRecord;
+            if (f != null) //Selected File
+            {
+                moveF = f;
+                moveD = null;
+            }
+            else //Selected Directory
+            {
+                moveF = null;
+                moveD = tvi;
+            }
+            MessageLabel.Text = "Select a bundle you wanna move to";
+            View1.Background = Brushes.LightYellow;
+        }
+
+        private void OnButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            var tvi = View1.SelectedItem as TreeViewItem;
+            if (tvi == null)
+                return;
+            var br = tvi.Tag as BundleRecord;
+            if (br != null) //Selected Bundle File
+            {
+                var fbd = new System.Windows.Forms.FolderBrowserDialog();
+                if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var fs = Directory.GetFiles(fbd.SelectedPath, "*", SearchOption.AllDirectories);
+                    var paths = ic.Hashes.Values;
+                    foreach (var f in fs)
+                    {
+                        var path = f.Remove(0, fbd.SelectedPath.Length + 1).Replace("\\","/");
+                        if (!paths.Contains(path))
+                        {
+                            MessageBox.Show("The index didn't define the file:" + Environment.NewLine + path, "Error");
+                            return;
+                        }
+                    }
+                    foreach (var f in fs)
+                    {
+                        var path = f.Remove(0, fbd.SelectedPath.Length + 1).Replace("\\", "/");
+                        var fr = ic.FindFiles[IndexContainer.FNV1a64Hash(path)];
+                        fr.Write(File.ReadAllBytes(f));
+                        fr.Move(br);
+                    }
+                    changed.Add(br);
+                    ButtonSave.IsEnabled = true;
+                    MessageBox.Show("Added " + fs.Length.ToString() + " files to " + br.Name, "Done");
+                }
+            }
+        }
+
+        private void MoveF(BundleRecord br)
+        {
+            if(MessageBox.Show("Are you sure you want to move " + ic.Hashes[moveF.Hash] + " into " + br.Name + "?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
+            {
+                changed.Add(moveF.bundleRecord);
+                changed.Add(br);
+                moveF.Move(br);
+                MessageBox.Show("Done!", "Done");
+                ButtonSave.IsEnabled = true;
+            }
+            moveF = null;
+            MessageLabel.Text = "";
+            View1.Background = Brushes.White;
+        }
+
+        private void MoveD(BundleRecord br)
+        {
+            if (MessageBox.Show("Are you sure you want to move directory " + ((TextBlock)((StackPanel)moveD.Header).Children[1]).Text + " into " + br.Name + "?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
+            {
+                MessageBox.Show("Moved " + MoveDir(moveD.Tag as Dictionary<string, TreeViewItem>, br).ToString() + " Files", "Done");
+                changed.Add(br);
+                ButtonSave.IsEnabled = true;
+            }
+            moveD = null;
+            MessageLabel.Text = "";
+            View1.Background = Brushes.White;
+        }
+
+        private int MoveDir(Dictionary<string, TreeViewItem> fis, BundleRecord br)
+        {
+            int count = 0;
+            foreach (var fi in fis)
+            {
+                var fr = fi.Value.Tag as FileRecord;
+                if (fr == null) // is directory
+                    count += MoveDir((Dictionary<string, TreeViewItem>)fi.Value.Tag, br);
+                else // is file
+                {
+                    fr.Move(br);
+                    changed.Add(fr.bundleRecord);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void OnButtonOpenClick(object sender, RoutedEventArgs e)
+        {
+            var tvi = View2.SelectedItem as TreeViewItem;
+            if (tvi == null)
+                return;
+            var f = tvi.Tag as FileRecord;
+            if (f != null) //Selected File
+            {
+                var path = Path.GetTempPath() + Path.DirectorySeparatorChar + Path.GetFileName(ic.Hashes[f.Hash]);
+                File.WriteAllBytes(path, f.Read());
+                Process.Start(path).Exited += OnProcessExit;
+            }
+        }
+
+        private void OnProcessExit(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete((sender as Process).StartInfo.FileName);
+            } 
+            catch (Exception) {}
+        }
+
+        private void OnButtonSaveClick(object sender, RoutedEventArgs e)
+        {
+            ButtonSave.IsEnabled = false;
+            foreach (var br in changed)
+                br.Save(br.Name);
+            ic.Save("_.index.bin");
+            MessageBox.Show("Success saved!" + Environment.NewLine + changed.Count.ToString() + " bundle files changed", "Done");
+            changed.Clear();
+        }
+
+        private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (ButtonSave.IsEnabled)
+                if (MessageBox.Show("There are unsaved changes" + Environment.NewLine + "Are you sure you want to leave?", "Closing", MessageBoxButton.OKCancel, MessageBoxImage.Warning, MessageBoxResult.Cancel) == MessageBoxResult.Cancel)
+                    e.Cancel = true;
+        }
+    }
+}
