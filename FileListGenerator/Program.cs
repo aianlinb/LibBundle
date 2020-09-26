f1a76a339e087a8ebf5f55bfef2958d53e39e9ad
aianlinb <ianlin0110g@gmail.com>
2020-09-26 16:52:05 +0800

    Add FileListGenerator
    The only requirement to use FileListGenerator is "_.index.bin"

---
+++ FileListGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using LibBundle;
+using System;
+using System.IO;
+
+namespace FileListGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("_.index.bin"))
+            {
+                Console.WriteLine("File not found: _.index.bin");
+                Console.WriteLine("Click enter to exit . . .");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Loading . . .");
+            var gc = new IndexContainer("_.index.bin");
+            var fl = File.CreateText("FileList.yml");
+            foreach (var b in gc.Bundles)
+            {
+                fl.WriteLine(b.Name + ":");
+                foreach (var f in b.Files)
+                    if (gc.Hashes.ContainsKey(f.Hash))
+                        fl.WriteLine("- " + gc.Hashes[f.Hash]);
+            }
+            fl.Flush();
+            fl.Close();
+            Console.WriteLine("Done!");
+            Console.WriteLine("Click enter to exit . . .");
+            Console.ReadLine();
+        }
+    }
+}
