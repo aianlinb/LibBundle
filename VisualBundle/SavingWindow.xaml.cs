f63122d5bf12d18041fa0ffa50062ddef939e93c
aianlinb <ianlin0110g@gmail.com>
2020-09-26 18:00:44 +0800

    Add missing file

---
+++ VisualBundle/SavingWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+namespace VisualBundle
+{
+    public partial class SavingWindow : Window
+    {
+        public SavingWindow()
+        {
+            InitializeComponent();
+            Closing += OnClosing;
+        }
+
+        internal void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
