7dcb9e528abc57ec05036457acd5d791b60c9693
aianlinb <ianlin0110g@gmail.com>
2020-09-28 00:43:25 +0800

    Optimize BundleExporter and fix DirectoryRecord reading

--- BundleExporter/Program.cs
+++ BundleExporter/Program.cs
@@ -33,29 +33,42 @@ namespace BundleExporter
             var ic = new LibBundle.IndexContainer("_.index.bin");
             Console.WriteLine("Found:");
             Console.WriteLine(ic.Bundles.Length.ToString() + " BundleRecords");
-            Console.WriteLine(ic.Directorys.Length.ToString() + " DirectoryRecords");
             Console.WriteLine(ic.Files.Length.ToString() + " FileRecords");
+            Console.WriteLine(ic.Directorys.Length.ToString() + " DirectoryRecords");
+            var ExistBundle = ic.Bundles.Where(o => File.Exists(o.Name));
+            Console.WriteLine(ExistBundle.Count().ToString() + " bundle.bin");
             Console.WriteLine("");
 
-            var ExistFiles = ic.Files.Where(o => File.Exists(o.bundleRecord.Name));
-            Console.Write("Exporting files . . . (");
             int count = 0;
-            var str = "/" + ExistFiles.Count().ToString() + ")";
-            foreach (var f in ExistFiles)
+            foreach (var b in ExistBundle)
+                count += b.Files.Count;
+            Console.Write("Exporting files . . . (");
+            var str = "/" + count.ToString() + ")";
+            count = 0;
+            foreach (var b in ExistBundle)
             {
-                count++;
-                Console.CursorLeft = 23;
-                Console.Write(count);
-                Console.Write(str);
-                try
+                var data = b.Bundle.Read();
+                foreach (var f in b.Files)
                 {
-                    var path = ic.Hashes[f.Hash];
-                    if (!Directory.Exists(Path.GetDirectoryName(path)))
+                    count++;
+                    Console.CursorLeft = 23;
+                    Console.Write(count);
+                    Console.Write(str);
+                    try
+                    {
+                        var path = ic.Hashes[f.Hash];
                         Directory.CreateDirectory(Path.GetDirectoryName(path));
-                    var fi = File.Create(path);
-                    fi.Write(f.Read(), 0, f.Size);
+                        var by = new byte[f.Size];
+                        data.Position = f.Offset;
+                        data.Read(by, 0, f.Size);
+                        File.WriteAllBytes(path, by);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine(e.ToString());
+                    }
                 }
-                catch (System.Collections.Generic.KeyNotFoundException) { }
+                data.Close();
             }
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("Click enter to exit . . .");
