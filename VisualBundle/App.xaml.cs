f24d64f752bd997e2d32997ddaf5803c6e81fe89
aianlinb <ianlin0110g@gmail.com>
2021-01-23 12:32:00 +0800

    Fix bundle saving

--- VisualBundle/App.xaml.cs
+++ VisualBundle/App.xaml.cs
@@ -1,26 +1,8 @@
-﻿using System.IO;
-using System.Windows;
+﻿using System.Windows;
 
 namespace VisualBundle
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            if (!File.Exists("LibBundle.dll"))
-            {
-                MessageBox.Show("File not found: LibBundle.dll", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                Shutdown();
-            }
-            if (!File.Exists("oo2core_8_win64.dll"))
-            {
-                MessageBox.Show("File not found: oo2core_8_win64.dll", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                Shutdown();
-            }
-        }
     }
 }
